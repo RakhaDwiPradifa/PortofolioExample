ce715a44a1d0e03667a2e7b33dafb509dd24e88b
RakhaDwiPradifa <rakhadwipradifa05@gmail.com>
2025-03-21 20:28:03 +0700

    First commit

---
+++ app.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="bg-[#DBDBDE] min-h-screen relative">
+    <section class="gap-32 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center bg-gradient-to-b from-[#FDFCFB] to-[#DBDBDE] outline-white  w-[90%] h-[90%] shadow-2xl rounded-3xl">
+<div class="flex justify-between w-[95%] mt-6">
+  <div class="flex gap-2">
+      <img class="bg-[#000000] rounded-full w-[25px] h-[25px]"
+      src="" alt="">
+      <h1 class="text-lg">RakhaDev</h1>
+    </div>
+
+    <div class="space-x-5 text-md">
+      <button>
+        <a href="http://">Home</a>
+      </button>
+      <button>
+        <a href="http://">About</a>
+      </button>
+      <button>
+        <a href="http://">Feature</a>
+      </button>
+      <button>
+        <a href="http://">Picking</a>
+      </button>
+      <button>
+        <a href="http://">Contact</a>
+      </button>
+    </div>
+
+    <div class="flex gap-4">
+      <button class="bg-[#000000] rounded-md w-[80px] h-[25px]">
+        <a class="text-white font-thin text-sm" 
+        href="http://">Get Started</a>
+      </button>
+
+      <img class="bg-[#000000] rounded-full w-[25px] h-[25px]"
+      src="" alt="">
+    </div>
+</div>
+    </section>
+    <div class="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+      s
+    </div>
+
+  </section>
+
+
+</template>
+
+<script setup>
+
+</script>
